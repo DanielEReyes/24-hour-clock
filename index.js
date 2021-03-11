f21a9bf28ec92417b7885550b3258a1b352f7b76
Daniel <77401608+DanielEReyes@users.noreply.github.com>
2021-03-11 16:16:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function currentTime() {
+  var date = new Date(); 
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  var milSec = date.getMilliseconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  milSec = updateTime(milSec);
+  oppMilSec = updateTime(999 - milSec)
+  oppSec = updateTime(60 - sec);
+  oppMin = updateTime(59 - min);
+  oppHour = updateTime(23 - hour);
+  document.getElementById("clock").innerHTML = hour + " : <span>" + min + "</span> : <span>"  + sec +  "</span> : <span>" + oppMilSec + "</span> : <span>" + oppSec + "</span> : <span>" + oppMin + "</span> : <span>" + oppHour;  
+   
+  var t = setTimeout(function() {currentTime()}, 100); 
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+
+
+currentTime();
